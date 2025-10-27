12fa3d516a4138b5a4c9ce3adf473153ddd99c1d
Nishant Singh <nsnishant@gmail.com>
2025-10-27 11:54:35 +0530

    Tabs layout setup

---
+++ todo-app/app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  linkButton: {
+    borderWidth: 2,
+    borderColor: 'blue',
+    borderRadius: 10,
+    padding: 16,
+  }
+})
--- todo-app/app/_layout.tsx
+++ todo-app/app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
+    </Stack>
+  );
+}
+/* export default function RootLayout() {
   return <Stack />;
 }
+ */
+
